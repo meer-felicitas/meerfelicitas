bd01a6881b550853d06c8273b8848e0bc9b807cb
binti-p <padaliya.binti@gmail.com>
2020-09-14 16:52:41 +0530

    add contactus page

--- meerfelicitas/urls.py
+++ meerfelicitas/urls.py
@@ -19,5 +19,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('contactus/', views.contactus, name='contactus')
 ]
